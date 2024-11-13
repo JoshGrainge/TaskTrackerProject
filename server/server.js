14715f0af48b9bc66a4528e5131b1a2d1b9c3b4e
Josh Grainge <josh.grainge@hotmail.com>
2024-11-13 17:29:38 -0500

    Updated app routes

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@ import "./config.js";
 import express from "express";
 import cors from "cors";
 import records from "./routes/records.js";
+import profile from "./routes/profile.js";
+import tasks from "./routes/tasks.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/record", records);
+app.use("/profile", records);
+app.use("/tasks", tasks);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
